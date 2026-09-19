d205c9316f6c96c2609c5b3ab4eef1850eeca268
Loacir-Zen <loacir.zb1990@gmail.com>
2026-09-19 16:45:58 -0300

    Modulo 8

---
+++ 8_inclusao_de_arquivo/4_short_tags/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "Testando código PHP nas tags normais <br>";
+
+?>
+
+
+<?
+
+    echo "Testando as shor tags <br>";    
+
+?>
---
+++ 8_inclusao_de_arquivo/5_exibicao_de_condeudo/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $nome = "Loacir";
+    $sobrenome = "Zen de Brito";
+
+?>
+
+
+<form action="">
+    <div>
+        <input type="text" value="<?= $nome; ?>">
+    </div>
+
+    <div>
+        <input type="text" value="<?= $sobrenome; ?>">
+    </div>
+    
+    <div>
+        <input type="submit" value="Enviar">
+    </div>
+
+</form>
